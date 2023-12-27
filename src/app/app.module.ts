3d257b101d984637dde8892272bb523422cdc8ef
Giovanni Roque <giovfranz@gmail.com>
2023-12-27 00:00:03 -0400

    front

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsuariosComponent,
+    TicketsComponent,
+    EquiposComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
