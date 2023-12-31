de033b02f3711dc36b011159eb83dfa6b8800b43
Giovanni Roque <giovfranz@gmail.com>
2023-12-31 16:58:36 -0400

    fron models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from "path";
+import {UsuariosComponent} from "./components/usuarios/usuarios.component";
+import {EquiposComponent} from "./components/equipos/equipos.component";
+import {TicketsComponent} from "./components/tickets/tickets.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'usuarios',component: UsuariosComponent},
+  {path: 'tickets',component: TicketsComponent},
+  {path: 'equipos',component: EquiposComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,24 @@
+export class Usuario {
+    id: number;
+    nombres: string;
+    paterno: string;
+    materno: string;
+    docIdentidad: string;
+    correoElectronico: string;
+    telefono: string;
+    rol: string;
+    estado: string;
+    constructor(id:number,nombres: string,paterno: string,materno: string,docIdentidad: string,correoElectronico: string,telefono: string,rol: string,estado: string){
+        this.id = id;
+        this.nombres = nombres;
+        this.paterno=paterno;
+        this.materno=materno;
+        this.docIdentidad=docIdentidad;
+        this.correoElectronico = correoElectronico;
+        this.telefono=telefono;
+        this.rol =rol;
+        this.estado = estado;
+    }
+
+
+    }
